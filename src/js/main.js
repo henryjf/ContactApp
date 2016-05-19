fe1989c26ac735a51f8bddad0b16776685a20731
JEFF HENRY <henry_jf@hotmail.com>
2016-05-18 22:22:58 -0400

    added angular in main.js/ started controllers

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,23 @@
 import angular from 'angular';
 
-angular
+//import  Controllers
+import { ListController } from './controllers/list.controller';
+import { SingleController } from './controllers/single.controller';
+import { AddController } from './controllers/add.controller';
 
+//import Configuration
+import { config } from './config';
+
+
+//Start Angular
+angular
+//setter for a module
   .module('app', []);
+  //register configuration
+  .config(config)
+
+  //register controllers
+  .controller('ListController', ListController)
+  .controller('SingleController', SingleController)
+  .controller('AddController', AddController)
+  ;
