92f55604a07c485a41706d187bd3ebef9ab40fb8
JEFF HENRY <henry_jf@hotmail.com>
2016-05-19 07:25:37 -0400

    renamed controller files

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 import angular from 'angular';
+import 'angular-ui-router';
 
 //import  Controllers
 import { ListController } from './controllers/list.controller';
@@ -8,15 +9,19 @@ import { AddController } from './controllers/add.controller';
 //import Configuration
 import { config } from './config';
 
+const url = 'https://secret-forest-21470.herokuapp.com/collections/3r4td/';
 
 //Start Angular
 angular
 //setter for a module
-  .module('app', []);
+  .module('app', ['ui.router'])
   //register configuration
   .config(config)
 
-  //register controllers
+  //app URL Constant
+  .constant('URL', url)
+
+  // register controllers
   .controller('ListController', ListController)
   .controller('SingleController', SingleController)
   .controller('AddController', AddController)
