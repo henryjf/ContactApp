13b7d0ade9550f81f6029f364f2481953beed140
JEFF HENRY <henry_jf@hotmail.com>
2016-05-21 13:55:46 -0400

    adding tiemplates

--- src/js/controllers/add.controller.js
+++ src/js/controllers/add.controller.js
@@ -3,12 +3,12 @@ function AddController ($scope, $http, URL, $state) {
 
   $scope.addContactInfo = function(contactObj) {
 
-    $http.post(URL,contactObj).then((res) => {
+    $http.post(URL, contactObj).then((res) => {
       console.log(res);
 
         $state.go('list');
     });
   }
 }
-AddController.$inject = ['$scope', '$http', URL]
+AddController.$inject = ['$scope', '$http', URL, '$state']
 export { AddController };
